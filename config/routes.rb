8fdd8e404587a1555f657a4b947ebe13271106d8
sarugakumiho <171490497+sarugakumiho@users.noreply.github.com>
2024-10-11 13:51:28 +0000

    [Add]deviseモデル

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  
+  # トップ画面に遷移
+  root to: 'homes#top'
+  
+  devise_for :admins
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
