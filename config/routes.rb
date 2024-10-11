5e6f3018b86dcafb45f1088f5ba5313c084d9fb6
sarugakumiho <171490497+sarugakumiho@users.noreply.github.com>
2024-10-11 14:35:28 +0000

    [Update]ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  
+  # トップページ
+  root to: 'homes#top'
+  # アバウトページ
+  get 'homes/about'
+  
+  # deviseルーティング
+  devise_for :admins
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
 end
