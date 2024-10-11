c163b41d3b8e95ebd1baaa168aadb28260591667
sarugakumiho <171490497+sarugakumiho@users.noreply.github.com>
2024-10-11 14:16:10 +0000

    [Add]homesコントローラ、ビュー

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   
-  # トップ画面に遷移
+  # トップページ
   root to: 'homes#top'
+  # アバウトページ
+  get 'homes/about'
   
+  # deviseルーティング
   devise_for :admins
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
