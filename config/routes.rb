c786c5aeb594028b7bad824562b469ea33f68cae
sarugakumiho <171490497+sarugakumiho@users.noreply.github.com>
2024-10-12 04:11:47 +0000

    [Add]Postコントローラ・ビュー

--- config/routes.rb
+++ config/routes.rb
@@ -9,13 +9,21 @@ Rails.application.routes.draw do
   devise_for :admins
   devise_for :users
   
-  # 会員ルーティング
+  # 会員側
   namespace :public do
+    # ユーザールーテイング
     resources :users, only: [:show, :index, :edit, :update, :destroy] do
       collection do
         get 'my_page'
         get 'check'
         patch 'information', to: 'users#update'
+      end
+    end
+    # 投稿ルーティング
+    resources :posts do
+      collection do
+        get 'tags'
+        get 'tags_search', to: 'posts#tags_search'
       end
     end
   end
