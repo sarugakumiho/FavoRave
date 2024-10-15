591a394bbaf7b0fc5e387e779542e509d20f41b8
sarugakumiho <171490497+sarugakumiho@users.noreply.github.com>
2024-10-15 11:04:37 +0000

    [Update]ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,30 @@ Rails.application.routes.draw do
   # homesルーティング
   root to: 'homes#top'
   get 'homes/about'
+  scope module: :public do
+    # membersルーティング
+    resources :members, only: [:show, :index] do
+      collection do
+        get '/my_page', to: 'members#my_page', as: 'my_page'
+      end
+      member do
+        get 'check'
+        delete 'destroy', to: 'members#destroy', as: 'destroy'
+      end
+      resources :posts, only: [:index, :show]
+    end
+    get '/members/information/edit', to: 'members#edit', as: 'edit_member_information'
+    patch '/members/information', to: 'members#update', as: 'update_member_information'
+    # postsルーティング
+    resources :posts, only: [:create, :new, :show, :index, :edit, :update, :destroy] do
+    # tag関係（一覧・検索）ルーティング
+      collection do 
+        get 'tags'
+      end
+    end
+    get '/posts/tags/search', to: 'posts#tags_search', as: 'posts_tags_search'
+  
+  end
   # ------------------------------------------------------------------------------------------------------------------
   # [Admin側]
   # homesルーティング
